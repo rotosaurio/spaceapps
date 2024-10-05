2bdb0cd1744cc69b1dfe475d30a728f0c350450d
rotosaurio <edgarafedo123@gmail.com>
2024-10-05 10:06:21 -0600

    login y index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Link href="/login">
+        <button>Iniciar sesión</button>
+      </Link>
     </div>
   );
 }
